d66a1833cf992e20fa47d5b562167b480dca8fc6
Christopher Maier <mail@christophermaier.com.au>
2018-05-07 19:56:26 +1000

    if statements

--- 01_Introduction/index.js
+++ 01_Introduction/index.js
@@ -1,2 +1,9 @@
-var a = 5 <= 7;
-console.log(a)
+var x = 10;
+
+if (x < 5) {
+  console.log("8 is greater");
+} else if (15 > x ) {
+  console.log("15 is greater!");
+} else {
+  console.log("8 is not greater");
+};
