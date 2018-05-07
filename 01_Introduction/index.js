98944d0eac3d747f3564f29164932770ce7bfbe0
Christopher Maier <mail@christophermaier.com.au>
2018-05-07 19:59:19 +1000

    if else

--- 01_Introduction/index.js
+++ 01_Introduction/index.js
@@ -1,9 +1,10 @@
-var x = 10;
-
-if (x < 5) {
-  console.log("8 is greater");
-} else if (15 > x ) {
-  console.log("15 is greater!");
-} else {
-  console.log("8 is not greater");
-};
+var check = function(number) {
+  if (number % 2 == 0) {
+    console.log(number + " is even");
+  } else {
+    console.log(number + " is odd");
+  }
+}
+check(4);
+check(7);
+check(111);
